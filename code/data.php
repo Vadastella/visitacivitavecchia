f64c9b17f7f0bee1a975fb310bf197448c17d749
EC2 Default User <ec2-user@ip-172-31-81-79.ec2.internal>
2020-03-29 13:54:14 +0000

    enter

--- code/data.php
+++ code/data.php
@@ -1,5 +1,42 @@
 <?php
 
-echo "{\"productList\": []}";
+$result = $_SERVER["REQUEST_TIME"];
+
+echo json_encode($result);
+return;
+
+$myObj = new stdClass();
+$myObj ->productList = array();
+
+$servername="visitacivitavecchia_db_1";
+$username="user";
+$password="password";
+$dbname="db";
+$conn=new mysqli($servername,$username,$password,$dbname);
+
+if($conn->connection_error){
+    die("Connection failed: ".$conn->connection_error);
+}
+
+$result =$conn->query
+("SELECT * FROM `products` WHERE 'name'= 'Cattedrale'");
+
+if($result->num_rows > 0){
+    foreach ($result as $row) {
+        $prod = new stdClass();
+        $prod->name = $row['name'];
+        $prod->name_subtitle = $row['name_subtitle'];
+        $myObj->productList[] = $prod;
+        $prod->video_url = $row['video_url'];
+        $prod->img1_url = $row['img1_url'];
+        $prod->img2_url = $row['img2_url'];
+        $prod->img3_url = $row['img3_url'];
+        $prod->txt1_url = $row['txt1_url'];
+        $prod->txt2_url = $row['txt2_url'];
+        $prod->txt3_url = $row['txt3_url'];
+    }
+}
+
+echo json_encode($myObj);
 
 ?>
